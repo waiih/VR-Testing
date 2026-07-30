d95256f2a574a562a835cea97ebf1e7aabff5bd1
miniaturity <miniaturity2.o@gmail.com>
2026-07-30 15:17:52 -0400

    please

--- Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
+++ Assets/Nokobot/Modern Guns - Handgun/_Demo Assets/SimpleShoot.cs
@@ -30,22 +30,12 @@ public class SimpleShoot : MonoBehaviour
             gunAnimator = GetComponentInChildren<Animator>();
     }
 
-    void Update()
-    {
-        //If you want a different input, change it here
-        if (Input.GetButtonDown("Fire1"))
-        {
-            //Calls animation on the gun that has the relevant animation events that will fire
-            gunAnimator.SetTrigger("Fire");
-            Shoot();
-            
-        }
-    }
-
 
     //This function creates the bullet behavior
-    void Shoot()
+    public void Shoot()
     {
+        gunAnimator.SetTrigger("Fire");
+
         if (muzzleFlashPrefab)
         {
             //Create the muzzle flash
